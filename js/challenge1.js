2792d70677cad0d6704270739485877493acf42d
jHastings23 <156715447+jHastings23@users.noreply.github.com>
2024-04-09 17:02:16 -0400

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,11 @@
+function chall(){
+    const time = new Date();
+    const currentTime = time.toLocaleString
+    alert("Current time:" +  currentTime);
+}
+document.addEventListener("dblclick", function()
+{
+    const time = new Date();
+    const currentTime = time.toLocaleString
+    alert("Current time:" +  currentTime);
+});
